3bcbd616a6b9b760a8b77ba5e3550c62d5d8df37
Matheus Souza <sn.matheushenrique@gmail.com>
2024-01-15 14:11:46 -0300

    Switching player each turn

--- src/application/UI.java
+++ src/application/UI.java
@@ -3,6 +3,7 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -48,6 +49,13 @@ public class UI {
 		}
 	}
 
+	public static void printMatch(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("Turn : " + chessMatch.getTurn());
+		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+	}
+
 	public static void printBoard(ChessPiece[][] pieces) {
 
 		for (int i = 0; i < pieces.length; i++) {
@@ -59,7 +67,7 @@ public class UI {
 		}
 		System.out.println("  a b c d e f g h");
 	}
-	
+
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 
 		for (int i = 0; i < pieces.length; i++) {
@@ -73,10 +81,10 @@ public class UI {
 	}
 
 	private static void printPiece(ChessPiece piece, boolean background) {
-		
-		if(background) {
+
+		if (background) {
 			System.out.print(ANSI_BLUE_BACKGROUND);
-		}	
+		}
 		if (piece == null) {
 			System.out.print("-" + ANSI_RESET);
 		} else {
