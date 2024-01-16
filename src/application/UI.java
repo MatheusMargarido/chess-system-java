a9b7e1eb2ce651d49d5770581e4aad0646cc7d32
Matheus Souza <sn.matheushenrique@gmail.com>
2024-01-16 15:33:28 -0300

    Knight

--- src/application/UI.java
+++ src/application/UI.java
@@ -122,7 +122,7 @@ public class UI {
 		System.out.print(ANSI_WHITE);
 		System.out.println(Arrays.toString(white.toArray()));
 		System.out.print(ANSI_RESET);
-		System.out.print("bLACK: ");
+		System.out.print("BLACK: ");
 		System.out.print(ANSI_YELLOW);
 		System.out.println(Arrays.toString(black.toArray()));
 		System.out.println(ANSI_RESET);
